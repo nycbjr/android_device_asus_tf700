6af72d49319dd7eb30c965a9bedb0c849b85021e
nycbjr <nybjr@gmail.com>
2012-10-13 16:41:36 -0400

    fixing vol key and clean up

--- device_tf700t.mk
+++ device_tf700t.mk
@@ -23,8 +23,8 @@ DEVICE_PACKAGE_OVERLAYS += device/asus/tf700t/overlay
 
 
 # This device is hdpi.
-PRODUCT_AAPT_CONFIG := xlarge hdpi mdpi
-PRODUCT_AAPT_PREF_CONFIG := hdpi
+##PRODUCT_AAPT_CONFIG := xlarge hdpi mdpi
+#PRODUCT_AAPT_PREF_CONFIG := hdpi
 #PRODUCT_LOCALES += hdpi
 
 # Prebuilt kernel location
