d754c2caa9d9d88aaa5128dbc4bd697858e00136
nycbjr <nycbjr@gmail.com>
2012-10-19 12:16:26 -0400

    tracing boot

--- device_tf700t.mk
+++ device_tf700t.mk
@@ -37,6 +37,7 @@ endif
 # Files needed for boot image
 PRODUCT_COPY_FILES += \
     $(LOCAL_KERNEL):kernel \
+    $(LOCAL_PATH)/ramdisk/init.rc:root/init.rc \
     $(LOCAL_PATH)/ramdisk/init.cardhu.rc:root/init.cardhu.rc \
     $(LOCAL_PATH)/ramdisk/init.cardhu.keyboard.rc:root/init.cardhu.keyboard.rc \
     $(LOCAL_PATH)/ramdisk/ueventd.cardhu.rc:root/ueventd.cardhu.rc \
