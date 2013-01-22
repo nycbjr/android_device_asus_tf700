fa27b16f0bcb9fc2d8e9e1e281040f6489d47b5f
nycbjr <nycbjr@gmail.com>
2013-01-22 11:05:38 -0500

    merging baked changes

--- asusdec/Android.mk
+++ asusdec/Android.mk
@@ -4,9 +4,9 @@
 # LOCAL_SRC_FILES := \
 #        $(call all-subdir-java-files)
 
-#LOCAL_MODULE_TAGS := optional
-#LOCAL_MODULE:= com.cyanogenmod.asusdec
-#include $(BUILD_JAVA_LIBRARY)
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE:= com.cyanogenmod.asusdec
+include $(BUILD_JAVA_LIBRARY)
 
-#include $(CLEAR_VARS)
-#include $(call all-makefiles-under,$(LOCAL_PATH))
+include $(CLEAR_VARS)
+include $(call all-makefiles-under,$(LOCAL_PATH))
