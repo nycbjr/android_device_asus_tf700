3205dbc59e566e90581ae56b6e7592906bb151f1
nycbjr <nycbjr@gmail.com>
2013-01-22 11:36:19 -0500

    fixing asusdev mk file

--- asusdec/Android.mk
+++ asusdec/Android.mk
@@ -1,8 +1,8 @@
-# LOCAL_PATH := $(call my-dir)
+ LOCAL_PATH := $(call my-dir)
 
-# include $(CLEAR_VARS)
-# LOCAL_SRC_FILES := \
-#        $(call all-subdir-java-files)
+ include $(CLEAR_VARS)
+ LOCAL_SRC_FILES := \
+        $(call all-subdir-java-files)
 
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE:= com.cyanogenmod.asusdec
